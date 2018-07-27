700b67a826a41d685e6e63fdcb4056d75b45d19c
Akito Ito <akito0107@gmail.com>
2018-07-27 17:20:32 +0900

    main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,21 +6,27 @@ import (
 	"net/http"
 	"os"
 	"fmt"
-	"runtime"
-	_ "net/http/pprof"
+		_ "net/http/pprof"
 
 	"github.com/akito0107/imgconvserver"
 	_ "github.com/akito0107/imgconvserver/engine/imaging"
+	"time"
+	"runtime/debug"
 )
 
 var configpath = flag.String("conf", "conf.toml", "config file path (default: conf.toml)")
 var port = flag.Int("port", 8080, "listen port")
 
 func main() {
-	runtime.GOMAXPROCS(4)
 	flag.Parse()
 	go func() {
 		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+	go func() {
+		t := time.NewTicker(10 * time.Second)
+		for range t.C {
+			debug.FreeOSMemory()
+		}
 	}()
 	f, err := os.Open(*configpath)
 	if err != nil {
